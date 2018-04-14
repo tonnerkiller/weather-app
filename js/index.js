977aba015cc5aeec67b79317488f491ddd68eddf
Benjamin Koppe <Benjamin.Koppe@gmx.de>
2018-04-14 11:04:32 +0200

    hidden buttons

--- js/index.js
+++ js/index.js
@@ -15,6 +15,27 @@ var geo_options = {
   timeout           : 27000
 };
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 /*fallback Weather is not at this moment used but remains for case of future test runs*/
 var fallbackWeather = {
     "coord": {
